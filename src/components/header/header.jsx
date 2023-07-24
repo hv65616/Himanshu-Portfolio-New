7c737bd32404de79888e4ad20e0e1aaea8f1d0b9
Himanshu <hv65616@gmail.com>
2023-07-24 14:58:49 +0530

    header component design

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-import "./header.css"
+import React from "react";
+import "./header.css";
+import Resume from "../../assests/resume.pdf";
+import Me from "../../assests/me7.png";
+import { BsLinkedin } from "react-icons/bs";
+import { BsGithub } from "react-icons/bs";
+import { BsInstagram } from "react-icons/bs";
 const header = () => {
   return (
-    <div>header</div>
-  )
-}
+    <header>
+      <div className="container header_container">
+        <h5>Hello I am</h5>
+        <h1>Himanshu Verma</h1>
+        <h5 className="text-light">Student | Programmer | Backend Developer</h5>
+        <div className="cta">
+          <a href={Resume} download className="btn">
+            Download Resume
+          </a>
+          <a href="#contact" className="btn btn-primary">
+            Let's Talk
+          </a>
+        </div>
+        <div className="header_socials">
+          <a href="https://www.linkedin.com" target="_blank" rel="noreferrer">
+            <BsLinkedin></BsLinkedin>
+          </a>
+          <a href="https://www.github.com" target="_blank" rel="noreferrer">
+            <BsGithub></BsGithub>
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+            <BsInstagram></BsInstagram>
+          </a>
+        </div>
+        <div className="me">
+          <img src={Me} alt="My_Image"></img>
+        </div>
+        <a href="#contact" className="scroll_down">
+          Scroll Down
+        </a>
+      </div>
+    </header>
+  );
+};
 
-export default header
+export default header;
